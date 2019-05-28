b4d8a95a55f5440b6a5c6241ff27ff37398e53a9
Marc Bouchez <theparaplux@gmail.com>
2019-05-28 16:25:09 +0200

    Cookie [Choix pro/part]

--- app/Http/Controllers/ChoixCategoryController.php
+++ app/Http/Controllers/ChoixCategoryController.php
@@ -8,26 +8,25 @@ class ChoixCategoryController extends Controller
 {
     public function pro ()
     {
-        $cookieKey = 'passerelle-numerique-choix-category'; 
-        $cookieDelay = - 1 * 60 * 24 * 365;
+        
+        $cookieKey = 'passerelle-numerique-choix-category';
 
         $cookieStatus = Cookie::get($cookieKey);
 
         if($cookieStatus === 'part') {
             Cookie::queue(Cookie::forget($cookieKey));
-        }
-        return back()->cookie($cookieKey, 'pro', $cookieDelay);
+        } 
+        return back()->cookie($cookieKey, 'pro', 1 * 60 * 24 * 365);
     }
     public function part ()
     {
-        $cookieKey = 'passerelle-numerique-choix-category'; 
-        $cookieDelay =  - 1 * 60 * 24 * 365;
+        $cookieKey = 'passerelle-numerique-choix-category';
 
         $cookieStatus = Cookie::get($cookieKey);
 
         if($cookieStatus === 'pro') {
             Cookie::queue(Cookie::forget($cookieKey));
         }
-        return back()->cookie($cookieKey, 'part', $cookieDelay);
+        return back()->cookie($cookieKey, 'part', 1 * 60 * 24 * 365);
     }
 }
