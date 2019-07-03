b7031c79be3ea1cb18c089f599aae55f770bfff6
Paraplux <43264146+Paraplux@users.noreply.github.com>
2019-07-03 14:21:40 +0200

    Quick Changes Nos Engagements

--- resources/views/nos-engagements.blade.php
+++ resources/views/nos-engagements.blade.php
@@ -10,53 +10,53 @@
 
 @section('content')
 <div class="page-engagement">
-<h1 class="title">Notre Charte d'Engagement</h1>
+<h1 class="title-1">Notre Charte d'Engagement</h1>
 <div class="banner-container">
     <img class="banniere" src="/images/icons/banniere-ordi.svg" alt="">
-    <h1 class="title-top">Passerelle</h1>
-    <h1 class="title-bot">Numerique</h1>
+    <h1 class="title-top title-2">Passerelle</h1>
+    <h1 class="title-bot title-2">Numerique</h1>
 </div>
 </div>
 
 <div class="article">
 <div class="article-v1 article">
-    <h1>Sensibiliser</h1>
+    <h1 class="title-2">Sensibiliser</h1>
     <i class="icon-1 fas fa-bullhorn"></i>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque illum labore ipsam voluptates est blanditiis veritatis quos, explicabo doloribus eum minus porro pariatur tempora? Accusantium, similique iste quas!</p>
 </div>
 <hr>
 <div class="article-v2 article">
-    <h1>Se Former</h1>
+    <h1 class="title-2">Se Former</h1>
     <i class="icon-2 fas fa-graduation-cap"></i>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos, unde minima! Repudiandae aut facilis blanditiis repellat libero vel sit sapiente veritatis tempore inventore! Natus voluptatum accusamus minima.</p>
 </div>
 <hr>
 <div class="article-v1 article">
-    <h1>Accompagner</h1>
+    <h1 class="title-2">Accompagner</h1>
     <i class="icon-3 far fa-handshake"></i>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci iste asperiores quibusdam dolor, officiis error et ipsum soluta unde eum corrupti temporibus corporis vero blanditiis! Adipisci asperiores maxime!</p>
 </div>
 <hr>
 <div class="article-v2 article">
-    <h1>Innover</h1>
+    <h1 class="title-2">Innover</h1>
     <i class="icon-4 far fa-lightbulb"></i>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda voluptatum culpa debitis quas quaerat repellendus nihil dolor! Deserunt, quod eius doloribus dicta suscipit voluptates optio, laudantium quam modi?</p>
 </div>
 <hr>
 <div class="article-v1 article">
-    <h1>Apprendre</h1>
+    <h1 class="title-2">Apprendre</h1>
     <i class="icon-5 fas fa-brain"></i>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci iste asperiores quibusdam dolor, officiis error et ipsum soluta unde eum corrupti temporibus corporis vero blanditiis! Adipisci asperiores maxime!</p>
 </div>
 <hr>
 <div class="article-v2 article">
-    <h1>Utiliser</h1>
+    <h1 class="title-2">Utiliser</h1>
     <i class="icon-6 far fa-hand-pointer"></i>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda voluptatum culpa debitis quas quaerat repellendus nihil dolor! Deserunt, quod eius doloribus dicta suscipit voluptates optio, laudantium quam modi?</p>
 </div>
 <hr>
 <div class="article-v1 article">
-    <h1>Partager</h1>
+    <h1 class="title-2">Partager</h1>
     <i class="icon-7 fas fa-hands"></i>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci iste asperiores quibusdam dolor, officiis error et ipsum soluta unde eum corrupti temporibus corporis vero blanditiis! Adipisci asperiores maxime!</p>
 </div>
